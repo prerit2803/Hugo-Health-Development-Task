01ac5fcb8c11890293ab2f513540e855d374dccc
prerit2803 <preritbhandari@gmail.com>
2018-04-05 22:39:24 -0400

    Code cleaning

--- index.js
+++ index.js
@@ -2,11 +2,9 @@ const CoinMarketCap = require('coinmarketcap-api')
 
 const client = new CoinMarketCap()
 const args = process.argv;
-// console.log("args",args[2]);
 var cryptoCurrency = args[2];
 if(cryptoCurrency){
   client.getTicker({limit: 1, currency: `${cryptoCurrency}`}).then(function(response){
-    // console.log("resp", response);
     if(response.error){
       console.error("Please run the program with valid crypto currency name: ", "docker run -e \"cryptoCurrencyName=<VALID-CRYPTO-CURRENCY-NAME>\" -it <DOCKER-IMAGE-NAME>");
     }else{
